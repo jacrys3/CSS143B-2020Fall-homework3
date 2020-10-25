58153eb732c731b4004162b03e7846c1ac1bef8a
Jacrys3 <ryse0528@gmail.com>
2020-10-25 00:22:30 -0700

    MinStack push works

--- src/main/java/Problem1/MinStack.java
+++ src/main/java/Problem1/MinStack.java
@@ -5,28 +5,38 @@ public class MinStack extends ArrayStack<Integer> {
     // can only use Stack interface and ArrayStack from this folder
     // do not use Java Stack
     private int min = 0;
+    private int index = 0;
     private int size;
 
     public MinStack(int size) {
         super(size);
-        this.size = size;
+        this.size = 0;
     }
 
     @Override
     public boolean push(Integer val) {
-        if(min > val) min = val;
+        if(size == 0) min = val;
+        else if(min > val){
+            min = val;
+            index++;
+        }
         size++;
         return super.push(val);
     }
 
     @Override
     public Integer pop() {
-        if(min == size) min = 0;
+        if(index == size){
+            index--;
+            min = this.peek();
+        }
         size--;
         return super.pop();
     }
 
     public Integer getMin() {
+        if(size == 0) return null;
+        else if(size == 1) return peek();
         return min;
     }
 }

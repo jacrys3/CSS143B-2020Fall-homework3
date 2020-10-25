92a1b15632f24aeb52dde3e6aa4238e2ac7d22e4
Jacrys3 <ryse0528@gmail.com>
2020-10-25 14:11:29 -0700

    GOT VALIDPARENTHESES LETS GO DUDE

--- src/main/java/Problem1/ValidParentheses.java
+++ src/main/java/Problem1/ValidParentheses.java
@@ -5,11 +5,11 @@ public class ValidParentheses {
 
     // Do not change signature (function name, parameters, return type)
     public static boolean isValid(String str) {
-
         if(str == null || str.length() == 0) return true;
         if(str.length() % 2 == 1) return false;
         ArrayStack data = new ArrayStack(str.length());
         char chr = str.charAt(0);
+
         if (chr == '(' || chr == '{' || chr == '[') data.push(chr);
         else return false;
 

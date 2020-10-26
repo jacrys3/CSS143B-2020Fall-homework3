25b79bc1d6d47de378f7e0c5f8b26fbca54cc29b
Jacrys3 <ryse0528@gmail.com>
2020-10-25 23:06:11 -0700

    Problem 3 now goes through correctly but prints wrong numbers

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -8,47 +8,18 @@ import java.io.PrintStream;
 
 public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
-        if(list == null) return;
         if(list.next == null){
-            print.println(list);
             return;
         }
-        int size = 0;
-        ListNode temp = list;
+        ListNode head = list;
+        list = list.next;
+        LinkedListStack data = new LinkedListStack();
         while(list.next != null){
-            temp = temp.next;
-            size++;
+            data.push(list.val);
         }
-        temp = list;
-        ArrayStack data = new ArrayStack(size);
-        while(data.peek() != null){
-            temp.next = (ListNode) data.peek();
-            temp = temp.next;
-            print.println((int) data.peek());
-            data.pop();
+        while(data.size() > 0){
+            print.println(data.pop());
         }
-        temp.next = null;
-
-        /*LinkedListStack data = new LinkedListStack();
-        ListNode ptr = list;
-        while(ptr.next != null){
-            data.push(ptr);
-            ptr = ptr.next;
-        }
-        list = ptr;
-        while(data.peek() != null){
-            ptr.next = (ListNode) data.peek();
-            ptr = ptr.next;
-            data.pop();
-        }
-        ptr.next = null;
-        printList(list, print);*/
-    }
-
-    private static void printList(ListNode data, PrintStream print){
-        while(data != null){
-            print.println(data.val);
-            data = data.next;
-        }
+        print.println(head.val);
     }
 }

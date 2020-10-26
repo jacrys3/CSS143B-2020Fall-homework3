27896eaa21f0d62e1c230db86ccdf6bb5cc64137
Jacrys3 <ryse0528@gmail.com>
2020-10-25 22:17:35 -0700

    minstack doesnt work

--- src/main/java/Problem1/MinStack.java
+++ src/main/java/Problem1/MinStack.java
@@ -15,10 +15,7 @@ public class MinStack extends ArrayStack<Integer> {
 
     @Override
     public boolean push(Integer val) {
-        if(super.size() == 0){
-            mini.push(val);
-        } else if((Integer) mini.peek() > val) {
-            mini.pop();
+        if(isMin(val)){
             mini.push(val);
         }
         return super.push(val);
@@ -26,13 +23,32 @@ public class MinStack extends ArrayStack<Integer> {
 
     @Override
     public Integer pop() {
-
+        if(getMin() == super.peek()){
+            super.pop();
+            mini.pop();
+            setMin(super.peek());
+        }
         return super.pop();
     }
 
+    private boolean isMin(int val){
+        if(super.size() == 0){
+            return true;
+        } else if((Integer) mini.peek() > val) {
+            mini.pop();
+            mini.push(val);
+            return true;
+        }
+        return false;
+    }
+    private void setMin(int min){
+        if(mini.size() == 0)
+            mini.push(min);
+    }
+
     public Integer getMin() {
-        if(mini.size() == 0) return null;
-        return (Integer) mini.peek();
+        Integer min = (Integer) mini.peek();
+        return min;
     }
 }
 
